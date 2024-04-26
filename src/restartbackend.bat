b48dc300bca36c3276cf3904cd242bd3b790562f
Rapa285 <prmahesa2004@gmail.com>
2024-04-27 06:51:54 +0700

    feat : fix run & restartbackend.bat

--- src/restartbackend.bat
+++ src/restartbackend.bat
@@ -1,4 +1,4 @@
 @echo off
 taskkill /f /im go.exe /t
 
-start cmd /k "cd wikiscrapper\backend && go run main.go prioqueue.go links.go ids.go ids_node.go node.go safemap.go ids_colly.go"
+start cmd /k "cd wikiscrapper\backend && go run main.go prioqueue.go links.go ids.go safemap.go ids_colly.go"
--- src/run.bat
+++ src/run.bat
@@ -2,5 +2,5 @@
 call ./stop.bat
 
 @REM ganti file dibawah pake main.go kalo udah kelar
-start cmd /k "cd wikiscrapper\backend && go run main.go prioqueue.go links.go ids.go ids_node.go node.go safemap.go ids_colly.go"
+start cmd /k "cd wikiscrapper\backend && go run main.go prioqueue.go links.go ids.go safemap.go ids_colly.go"
 start cmd /k "cd wikiscrapper && npm run dev"

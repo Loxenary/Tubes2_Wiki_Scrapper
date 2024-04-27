117fac03926050b5c9d58501c9de46c33d13a149
Rapa285 <prmahesa2004@gmail.com>
2024-04-27 23:03:14 +0700

    feat : add safemap for depth

--- src/wikiscrapper/backend/ids.go
+++ src/wikiscrapper/backend/ids.go
@@ -19,23 +19,17 @@ func IDS(startURL, targetURL string, depthLimit int, counter *int) []string {
 	} else if depthLimit <= 1 { // Error Handling , Jika depth == 1 berarti mengecek apakah start == target karena sudah dicek maka return nil)
 		return nil
 	}
+	visited := *NewDepthSafeMap()
 	for depth := 2; depth <= depthLimit; depth++ { //mulai loop dari depth == 2 karena depth 1 sudah dicek dan depth < 1 -> nil
  
 		// Membuat SafeMap baru pada setiap loop
-		visited := *NewSafeMap()
+		
 
 
 		fmt.Println("depth", depth)
 
 		// Start Timer untuk dls melihat waktu yang diperlukan setiap depth
 		start := time.Now()
-<<<<<<< HEAD
-
-		path = DLS(startURL, targetURL, depth,&visited, &mutex, counter)
-		fmt.Println("DLS Time", depth, ":", time.Since(start)) // Output runtime depth ke-depth
-		fmt.Println(path) 
-		if path != nil { //Apabila path != []
-=======
 		var wg sync.WaitGroup
 		path = DLS(startURL, targetURL, depth,&visited, &mutex, counter, &wg)
 		fmt.Println("DLS Time", depth, ":", time.Since(start))
@@ -43,7 +37,6 @@ func IDS(startURL, targetURL string, depthLimit int, counter *int) []string {
 		
 		fmt.Println(path)
 		if path != nil {
->>>>>>> 0583096ff45f1c89931eb5b94d4c403194db6935
 			return path
 		}
 		fmt.Println("Not Found")
@@ -53,15 +46,10 @@ func IDS(startURL, targetURL string, depthLimit int, counter *int) []string {
 	return nil
 }
 
-<<<<<<< HEAD
-// DLS concurrent
-func DLS(currentURL, targetURL string, depthLimit int, visited *SafeMap, mutex *sync.Mutex, counter *int) []string {
-=======
 // Performing Depth limited search concurrently
-func DLS(currentURL, targetURL string, depthLimit int, visited *SafeMap, mutex *sync.Mutex, counter *int, wg *sync.WaitGroup) []string {
->>>>>>> 0583096ff45f1c89931eb5b94d4c403194db6935
+func DLS(currentURL, targetURL string, depthLimit int, visited *DepthSafeMap, mutex *sync.Mutex, counter *int, wg *sync.WaitGroup) []string {
 
-	if visited.Get(currentURL) { // Mengecek apakah sudah dikunjungi (nil jika sudah)
+	if visited.Get(depthLimit,currentURL) { // Mengecek apakah sudah dikunjungi (nil jika sudah)
 		fmt.Println("flag visited")
 		return nil
 	}
@@ -70,22 +58,14 @@ func DLS(currentURL, targetURL string, depthLimit int, visited *SafeMap, mutex *
 		return []string{currentURL}
 	}
 
-<<<<<<< HEAD
-	visited.Set(currentURL, true) // Mengset dikunjungi -> true
-=======
 	fmt.Println("Current URL: ",currentURL, " depth: ",depthLimit);
 
-	visited.Set(currentURL, true)
->>>>>>> 0583096ff45f1c89931eb5b94d4c403194db6935
+	visited.Set(depthLimit,currentURL, true)
 	mutex.Lock()
 	(*counter)++ // Menambah jumlah article yang dikunjungi
 	mutex.Unlock()
 
-<<<<<<< HEAD
-	links, found := getListofLinks1(targetURL, currentURL, *visited) // Mengambil URL dari currentURL
-=======
 	links, found := LinksProcessor(targetURL, currentURL)
->>>>>>> 0583096ff45f1c89931eb5b94d4c403194db6935
 
 	if found {
 		fmt.Println("found target from", currentURL)
@@ -93,28 +73,14 @@ func DLS(currentURL, targetURL string, depthLimit int, visited *SafeMap, mutex *
 	} else if !found && depthLimit == 2 {  // Karena selanjutnya depth == depthLimit -1 == 1 , dan dalam links tidak ada target maka nil
 		return nil
 	}
-<<<<<<< HEAD
-
-	// variable wait group untuk DLS selanjutnya
-	var wg sync.WaitGroup 
-
-	for _, link := range links { //loop dari URL yang didapat, dimulai dari yang paling awal 
-=======
 	for _, link := range links {
->>>>>>> 0583096ff45f1c89931eb5b94d4c403194db6935
 
 		wg.Add(1) 
 		worker := func(link string) []string { //Menambah go routine
 			defer wg.Done()
-<<<<<<< HEAD
-			subPath := DLS(link, targetURL, depthLimit-1, visited, mutex, counter) //Recursive
-			if subPath != nil {
-				return append([]string{currentURL}, subPath...) // Mengembalikan [currentURL,hasil DLS...]
-=======
 			subPath := DLS(link, targetURL, depthLimit-1, visited, mutex, counter, wg)
 			if subPath != nil {
 				return append([]string{currentURL}, subPath...)
->>>>>>> 0583096ff45f1c89931eb5b94d4c403194db6935
 			}
 			return nil //jika hasil DLS []
 		}(link)
--- src/wikiscrapper/backend/safemap.go
+++ src/wikiscrapper/backend/safemap.go
@@ -29,3 +29,31 @@ func (sm *SafeMap) Get(key string) bool {
     defer sm.mu.Unlock()
     return sm.visited[key]
 }
+
+type DepthSafeMap struct {
+    mu sync.Mutex
+    visited map[int]map[string]bool
+}
+
+// Initiate a new SafeMap
+func NewDepthSafeMap() *DepthSafeMap {
+    return &DepthSafeMap{visited: make(map[int]map[string]bool)}
+}
+
+// Set the value of a key in the map
+func (sm *DepthSafeMap) Set(depth int,key string, value bool) {
+    sm.mu.Lock()
+    defer sm.mu.Unlock()
+    if sm.visited[depth] == nil {
+        sm.visited[depth] = make(map[string]bool)
+    }
+    sm.visited[depth][key] = value
+}
+
+// Get the value of a key in the map
+func (sm *DepthSafeMap) Get(depth int,key string) bool {
+    sm.mu.Lock()
+    defer sm.mu.Unlock()
+    temp := sm.visited[depth]
+    return temp[key]
+}
